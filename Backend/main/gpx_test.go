97cf0d0ad99a037261078f1f061faf09b73fce2e
andii04 <36232071+andii04@users.noreply.github.com>
2019-12-30 17:01:54 +0100

    Der Test funzt iwie net, keine ahnung warum
    Diese Art von Test in eigenem kleinen Projekt lief so auf die Art und Weise

--- Backend/main/gpx_test.go
+++ Backend/main/gpx_test.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
+	"math"
 	"testing"
 )
 
-//Beispiel
-func TestSomething(t *testing.T) {
-	// assert equality
-	assert.Equal(t, 123, 123, "they should be equal")
-	// assert inequality
-	assert.NotEqual(t, 123, 456, "they should not be equal")
-	// assert for nil (good for errors)
-	//	assert.Nil(t, object)
-	// assert for not nil (good when you expect something)
-	//	if assert.NotNil(t, object) {
-
-	// now we know that object isn't nil, we are safe to make
-	// further assertions without causing any errors
-	//		assert.Equal(t, "Something", object.Value)
-
-	//	}
-
+//Beispiel (Test funzt nicht, warum auch immer ?!)
+func TestToRad(t *testing.T) {
+	var res = ToRad(180)
+	if res != math.Pi {
+		t.Errorf("Sum was inccorect, got: %f, wanted: %f", res, math.Pi)
+	}
 }
