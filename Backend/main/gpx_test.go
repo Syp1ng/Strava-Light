44cc89e9720d0f9d8aa101515d0a3439b6afaa5d
Wörrlein, Andreas <Andreas.Woerrlein@tecalliance.net>
2020-01-02 17:46:46 +0100

    testing dist betweentopoints

--- Backend/main/gpx_test.go
+++ Backend/main/gpx_test.go
@@ -12,3 +12,10 @@ func TestToRad(t *testing.T) {
 		t.Errorf("Sum was inccorect, got: %f, wanted: %f", res, math.Pi)
 	}
 }
+
+func TestDistanceBetweenToPoints(t *testing.T) {
+	var result = distanceBetweenTwoPoints(49.3547198000, 9.1508659200, 49.3546998700, 9.1509324100)
+	if result != 5.301269557828212 {
+		t.Errorf("Distance was wrong, got: %f, wanted %f", result, 5.301269557828212)
+	}
+}
