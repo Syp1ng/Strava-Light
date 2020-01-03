2c257028b51da56120ea6d0a31290bee263fa822
fabian.schurk@web.de <Fabi03071999>
2020-01-03 01:42:13 +0100

    noch weniger Plan

--- Backend/main/UploadFileHandler_test.go
+++ Backend/main/UploadFileHandler_test.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
+	"log"
+	"strconv"
+
 	//"github.com/stretchr/testify/assert"
 	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -10,4 +15,20 @@ func TestUploadFile(t *testing.T) {
 	tempFile, _ := ioutil.TempFile("DataStorage/GPX_Files", "gpxDatei*.gpx")
 	uploadfile(tempFile.Name(), "Laufen", "Kommentar", 1)
 	//assert.
+	activityDataLenght, error := os.Open(dbLocationActivity)
+	if error == nil {
+		readerforlengh := csv.NewReader(activityDataLenght)
+		for {
+			lin, err := readerforlengh.Read()
+			if err == nil {
+				if lin[2] == //da den PFadlalala {
+					//dann is in actdb drin
+				}
+
+			} else {
+				break
+				log.Println(lin)
+			}
+		}
+	}
 }
