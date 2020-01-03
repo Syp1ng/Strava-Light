8c695464e7d00d0df0b6e28f060abf0f9e2ebcbb
andii04 <36232071+andii04@users.noreply.github.com>
2020-01-03 01:26:03 +0100

    komische funktion

--- Backend/main/UploadFileHandler_test.go
+++ Backend/main/UploadFileHandler_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	//"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"testing"
 )
 
 func TestUploadFile(t *testing.T) {
-
+	tempFile, _ := ioutil.TempFile("DataStorage/GPX_Files", "gpxDatei*.gpx")
+	uploadfile(tempFile.Name(), "Laufen", "Kommentar", 1)
+	//assert.
 }
