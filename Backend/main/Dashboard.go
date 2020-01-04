7cb7f1b8f93b0f83c8321a909de4b25747e2e973
DennisWohlfarth <den.wohlfarth.17@lehre.mosbach.dhbw.de>
2020-01-04 13:06:24 +0100

    Remove and Edit works

--- Backend/main/Dashboard.go
+++ Backend/main/Dashboard.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
@@ -36,6 +37,7 @@ func getDataForUser(uID int) []Activity {
 			}
 		}
 	}
+	activityDataLenght.Close()
 	activityData, err := os.Open(dbLocationActivity)
 	if err == nil {
 		reader := csv.NewReader(activityData)
@@ -67,8 +69,10 @@ func getDataForUser(uID int) []Activity {
 				break
 			}
 		}
+		activityData.Close()
 		return slice
 	}
+	activityData.Close()
 	return nil
 
 }
@@ -98,11 +102,77 @@ func removeActivity(uID int, activityID int) {
 			//log.Println("Not allowed to delete")
 		}
 	}
-	os.Rename("Temp.csv", "TEST.csv") //funktiniert nicht ??
-
+	file.Close()
+	f.Close()
+	var error = os.Rename(filepath.Join("DataStorage", "ActivityDB.csv"), filepath.Join("DataStorage", "BackupActivityDB.csv"))
+	if error != nil {
+		log.Println(error)
+	} else {
+		error = os.Rename(filepath.Join("DataStorage", "Temp.csv"), filepath.Join("DataStorage", "ActivityDB.csv"))
+		if error != nil {
+			log.Println(error)
+		}
+	}
+	//os.Remove(path)
 }
-func editActivity(activity Activity) {
-	fmt.Println(activity.Comment + " " + activity.Activityart)
+
+func editActivity(editactivity Activity) {
+	file, err := os.Open(dbLocationActivity)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	f, err := os.OpenFile("DataStorage/Temp.csv", os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), ",")
+		actID, err := strconv.Atoi(line[0])
+		//userID, err := strconv.Atoi(activity[1])
+		if err != nil {
+
+		}
+		if actID != editactivity.ActID {
+			f.WriteString(scanner.Text() + "\n")
+
+		} else {
+			editactivity.Filename = line[2]
+			editactivity.Distance, err = strconv.ParseFloat(line[5], 64)
+			editactivity.Standzeit, err = strconv.ParseFloat(line[6], 64)
+			editactivity.HighSpeed, err = strconv.ParseFloat(line[7], 64)
+			editactivity.Highspeedtime = line[8]
+			editactivity.Avgspeed, err = strconv.ParseFloat(line[9], 64)
+			editactivity.AvgSpeedFastKM, err = strconv.Atoi(line[10])
+			editactivity.AvgSpeedFastMS, err = strconv.ParseFloat(line[11], 64)
+			editactivity.AvgSpeedSlowKM, err = strconv.Atoi(line[12])
+			editactivity.AvgSpeedSlowMS, err = strconv.ParseFloat(line[13], 64)
+		}
+	}
+	var newline = strconv.Itoa(editactivity.ActID) + "," + strconv.Itoa(editactivity.UserID) + "," + editactivity.Filename + "," +
+		editactivity.Activityart + "," + editactivity.Comment + "," + fmt.Sprintf("%f", editactivity.Distance) + "," + fmt.Sprintf("%f", editactivity.Standzeit) + "," +
+		fmt.Sprintf("%f", editactivity.HighSpeed) + "," + editactivity.Highspeedtime + "," + fmt.Sprintf("%f", editactivity.Avgspeed) +
+		"," + strconv.Itoa(editactivity.AvgSpeedFastKM) + "," + fmt.Sprintf("%f", editactivity.AvgSpeedFastMS) + "," +
+		strconv.Itoa(editactivity.AvgSpeedSlowKM) + "," + fmt.Sprintf("%f", editactivity.AvgSpeedSlowMS) +
+		"\n"
+	_, err = f.WriteString(newline)
+	if err != nil {
+		log.Println(err)
+	}
+
+	file.Close()
+	f.Close()
+	var error = os.Rename(filepath.Join("DataStorage", "ActivityDB.csv"), filepath.Join("DataStorage", "BackupActivityDB.csv"))
+	if error != nil {
+		log.Println(error)
+	} else {
+		error = os.Rename(filepath.Join("DataStorage", "Temp.csv"), filepath.Join("DataStorage", "ActivityDB.csv"))
+		if error != nil {
+			log.Println(error)
+		}
+	}
 }
 func saveNewData(activity []Activity) {
 
