f90aa98a73a52f9d04f4369195f8329a0fc7da12
Syp1ng <36132762+Syp1ng@users.noreply.github.com>
2020-01-12 17:20:46 +0100

    nearly done

--- Backend/main/main.go
+++ Backend/main/main.go
@@ -7,15 +7,28 @@ import (
 )
 
 func main() {
-	fmt.Println("Webserver Starting")
 	allSessions = make(map[string]sessionKeyInfo)
 	userDataMap = make(map[int]userData)
 	activityMap = make(map[int]Activity)
 	activityMapForUser = make(map[int]Activity)
+	/*
+		port = ":" + strconv.Itoa(*flag.Int("port", 443, "Port for Webserver"))
+		saltLen = *flag.Int("saltLen", 10, "Length of the salt for the Password")
+		sessionKeyLen = *flag.Int("sessionKeyLen", 50, "Length of the session Key")
+	*/
 
-	port = ":" + strconv.Itoa(*flag.Int("port", 443, "Port for Webserver"))
-	saltLen = *flag.Int("saltLen", 10, "Length of the salt for the Password")
-	sessionKeyLen = *flag.Int("sessionKeyLen", 50, "Length of the session Key")
+	//this way because of defining settings in main
+	flagPort := flag.Int("port", 443, "Port for Webserver")
+	flagSaltLen := flag.Int("saltLen", 10, "Length of the salt for the Password")
+	flagSessionKeyLen := flag.Int("sessionKeyLen", 50, "Length of the session Key")
+
+	flag.Parse()
+
+	port = ":" + strconv.Itoa(*flagPort)
+	saltLen = *flagSaltLen
+	sessionKeyLen = *flagSessionKeyLen
+
+	fmt.Println("Webserver Starting on port" + port)
 
 	SetupLinks()
 }
